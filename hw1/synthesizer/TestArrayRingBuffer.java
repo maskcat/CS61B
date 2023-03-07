9588ecab2d756cf5721f934e714d186ce70e5a67
maskcat <fymaskcat@outlook.com>
2023-03-07 16:32:11 +0800

    hw1

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,11 +9,46 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(10);
+        arb.enqueue(1);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        assertEquals(arb.fillCount(),3);
+        int de = arb.dequeue();
+        assertEquals(de,1);
+        assertEquals(arb.fillCount(),2);
+        arb.enqueue(4);
+        assertEquals(arb.fillCount(),3);
+        int de1 = arb.dequeue();
+        int de2 = arb.dequeue();
+        int de3 = arb.dequeue();
+        assertEquals(de1,2);
+        assertEquals(de2,3);
+        assertEquals(de3,4);
+        assertTrue(arb.isEmpty());
+        assertFalse(arb.isFull());
+        arb.enqueue(1);
+        arb.enqueue(1);
+        arb.enqueue(1);
+        arb.enqueue(1);
+        arb.enqueue(1);
+        arb.enqueue(1);
+        arb.enqueue(1);
+        arb.enqueue(1);
+        arb.enqueue(1);
+        arb.enqueue(1);
+        for (Integer integer : arb) {
+            assertEquals(1,integer.intValue());
+        }
+        assertTrue(arb.isFull());
+        assertFalse(arb.isEmpty());
+
+
+
     }
 
     /** Calls tests for ArrayRingBuffer. */
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
     }
-} 
+}
